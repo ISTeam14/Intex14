c0f4ba56bd2425e164c8a87256f04b520a3d7d53
Jessica Lasson <jessicawood4456@gmail.com>
2025-04-05 17:59:21 -0600

    Initial blank app!

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Good Luck on Intex Team 14!!</h1>
+      <h2>We got this!!!</h2>
+    </>
+  );
+}
+
+export default App;
