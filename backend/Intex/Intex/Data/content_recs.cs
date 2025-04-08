2c24fb2962e895b16989a4449ccac171f1eda9be
kmanstu9 <kmanstu9@byu.edu>
2025-04-08 10:19:49 -0600

    Add content_recs model with relationships to movies_titles

---
+++ backend/Intex/Intex/Data/content_recs.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intex.Data
+{
+    public class content_recs
+    {
+        [Key]
+        public int Id { get; set; }  // Primary key
+
+        [ForeignKey("BaseMovie")]
+        public string base_show_id { get; set; }
+
+        [ForeignKey("RecommendedMovie")]
+        public string recommended_show_id { get; set; }
+
+        public double similarity_score { get; set; }
+
+        // Navigation properties
+        public movies_titles BaseMovie { get; set; }
+        public movies_titles RecommendedMovie { get; set; }
+    }
+}
