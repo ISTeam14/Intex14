b0ea203f151cc642624deebf0a0c2abba4065a80
Dauce <126361698+Dauce@users.noreply.github.com>
2025-04-08 11:06:57 -0600

    frontend security changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MoviePage from './pages/MoviePage';
+import LoginPage from './pages/LoginPage';
+import Register from './pages/RegisterPage';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/movie" element={<MoviePage />} />
         </Routes>
