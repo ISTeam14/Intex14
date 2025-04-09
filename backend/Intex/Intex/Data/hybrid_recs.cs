a187ed4559ebb2f076528f344c4e50263386b962
kmanstu9 <kmanstu9@byu.edu>
2025-04-09 11:02:23 -0600

    Replace content_recs with hybrid_recs and update MovieDbContext

--- backend/Intex/Intex/Data/hybrid_recs.cs
+++ backend/Intex/Intex/Data/hybrid_recs.cs
@@ -3,23 +3,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Intex.Data
 {
-    public class content_recs
+    public class hybrid_recs
     {
         [Key]
         public int Id { get; set; }
 
         [Required]
-        [ForeignKey("BaseMovie")]
+        [Column("base_show_id")]
         public string base_show_id { get; set; }
 
         [Required]
-        [ForeignKey("RecommendedMovie")]
+        [Column("recommended_show_id")]
         public string recommended_show_id { get; set; }
 
         [Required]
+        [Column("similarity_score")]
         public double similarity_score { get; set; }
 
+        [ForeignKey("base_show_id")]
         public movies_titles BaseMovie { get; set; }
+
+        [ForeignKey("recommended_show_id")]
         public movies_titles RecommendedMovie { get; set; }
     }
 }
